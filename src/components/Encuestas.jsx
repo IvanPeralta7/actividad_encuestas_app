a9b380ab396e38c2a295801e6b4157571fe9d993
Ivan peralta <ivanperalta_cba2013@live.com>
2023-09-22 14:45:15 -0300

    Ejercicio 4

---
+++ src/components/Encuestas.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./components.css"
+
+function Encuestas({ encuestas }) {
+  return (
+      <div>
+        <h2>Encuestas Disponibles</h2>     
+         {encuestas.map((encuesta) => (
+            <ul key={encuesta.id}>
+                <li>{encuesta.pregunta}</li>
+                    {encuesta.opciones.map((opcion) => (
+                    <div>
+                        <input key={opcion} id={opcion} type="radio"/>
+                            <label>{opcion}</label>
+                    </div>
+                    ))}
+            </ul>
+        ))}
+      </div>
+  );
+}
+
+export default Encuestas;
